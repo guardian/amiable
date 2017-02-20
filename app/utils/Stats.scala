7304d46b7624bedc8c2b518740e1975ffad7cc2f
Thomas Bonnin <thomas.bonnin@gmail.com>
2017-02-20 11:57:16 +0000

    Int rather than double to express percentiles and handle empty seq

--- app/utils/Stats.scala
+++ app/utils/Stats.scala
@@ -2,18 +2,22 @@ package utils
 
 case class Percentiles(values: Seq[Int]) {
 
-  private def percentile(s: Seq[Int], p: Double): Int = {
-    if(p < 0 || p > 1) throw new RuntimeException("Percentile rank should be between 0 and 1")
-    s.sortWith(_ < _)(Math.ceil((s.length - 1) * p).toInt)
+  private val percentilesRange: Range = 0 to 100
+  private def percentile(s: Seq[Int], p: Int): Option[Int] = {
+    if(p < percentilesRange.min || p > percentilesRange.max) None
+    s.sortWith(_ < _) match {
+      case Nil => None
+      case nonEmptySeq => Some(nonEmptySeq(Math.ceil((s.length - 1) * p / percentilesRange.max).toInt))
+    }
   }
 
-  def p95 = percentile(values, 0.95)
-  def p90 = percentile(values, 0.90)
-  def p75 = percentile(values, 0.75)
-  def p50 = percentile(values, 0.50)
-  def p25 = percentile(values, 0.25)
-  def lowest = percentile(values, 0)
-  def highest = percentile(values, 1)
+  def p95 = percentile(values, 95)
+  def p90 = percentile(values, 90)
+  def p75 = percentile(values, 75)
+  def p50 = percentile(values, 50)
+  def p25 = percentile(values, 25)
+  def lowest = percentile(values, percentilesRange.min)
+  def highest = percentile(values, percentilesRange.max)
 
 }
 
--- test/utils/StatsTest.scala
+++ test/utils/StatsTest.scala
@@ -9,26 +9,30 @@ class StatsTests extends FreeSpec with Matchers {
   val percentiles = Percentiles(Random.shuffle(0 to 100).toSeq)
 
   "Percentiles" - {
+    "should return None if no values" in {
+      Percentiles(Seq.empty[Int]).p25 shouldEqual None
+    }
+
     "should correctly calculate 95th percentile" in {
-      percentiles.p95 shouldEqual 95
+      percentiles.p95 shouldEqual Some(95)
     }
     "should correctly calculate 90th percentile" in {
-      percentiles.p90 shouldEqual 90
+      percentiles.p90 shouldEqual Some(90)
     }
     "should correctly calculate 75th percentile" in {
-      percentiles.p75 shouldEqual 75
+      percentiles.p75 shouldEqual Some(75)
     }
     "should correctly calculate 50th percentile" in {
-      percentiles.p50 shouldEqual 50
+      percentiles.p50 shouldEqual Some(50)
     }
     "should correctly calculate 25th percentile" in {
-      percentiles.p25 shouldEqual 25
+      percentiles.p25 shouldEqual Some(25)
     }
     "should correctly calculate lowest value" in {
-      percentiles.lowest shouldEqual 0
+      percentiles.lowest shouldEqual Some(0)
     }
     "should correctly calculate highest value" in {
-      percentiles.highest shouldEqual 100
+      percentiles.highest shouldEqual Some(100)
     }
   }
 }
