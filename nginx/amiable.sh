6e3baf65324ca11fd7cba8769a8b896e7c55506f
Philip McMahon <philip.mcmahon@guardian.co.uk>
2018-01-11 17:21:56 +0000

    simplify nginx script

--- nginx/amiable.sh
+++ nginx/amiable.sh
@@ -5,14 +5,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 nginxHome=`nginx -V 2>&1 | grep "configure arguments:" | sed 's/[^*]*conf-path=\([^ ]*\)\/nginx\.conf.*/\1/g'`
 
-SYSTEM=$(uname -s)
-if [ $SYSTEM == "Darwin" ]; then
-    # Mac OS X platform
-    confDir=servers
-elif [ $SYSTEM == "Linux" ]; then
-    confDir=sites-enabled
-fi
-
 sudo ln -fs $DIR/amiable.conf $nginxHome/sites-enabled/amiable.conf
 sudo ln -fs $DIR/amiable.crt $nginxHome/amiable.crt
 sudo ln -fs $DIR/amiable.key $nginxHome/amiable.key
