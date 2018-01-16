2d008929ca15e7c8bf630cea0d464e3a6dd386d6
Philip McMahon <philip.mcmahon@guardian.co.uk>
2018-01-16 10:15:44 +0000

    Only use sites-enabled as nginx dir if exists.

--- nginx/amiable.sh
+++ nginx/amiable.sh
@@ -5,7 +5,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 nginxHome=`nginx -V 2>&1 | grep "configure arguments:" | sed 's/[^*]*conf-path=\([^ ]*\)\/nginx\.conf.*/\1/g'`
 
-sudo ln -fs $DIR/amiable.conf $nginxHome/sites-enabled/amiable.conf
+if [ -d "${nginxHome}/sites-enabled" ]; then
+  confDir=sites-enabled
+else
+  confDir=servers
+fi
+
+sudo ln -fs $DIR/amiable.conf $nginxHome/$confDir/amiable.conf
 sudo ln -fs $DIR/amiable.crt $nginxHome/amiable.crt
 sudo ln -fs $DIR/amiable.key $nginxHome/amiable.key
 sudo nginx -s stop
