032271c16b7f3163bd6132e363b7aa9f27eeda1b
Philip McMahon <philip.mcmahon@guardian.co.uk>
2018-01-17 11:18:31 +0000

    Add docs to nginx script.

--- nginx/amiable.sh
+++ nginx/amiable.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # setup nginx for local development
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 nginxHome=`nginx -V 2>&1 | grep "configure arguments:" | sed 's/[^*]*conf-path=\([^ ]*\)\/nginx\.conf.*/\1/g'`
 
@@ -12,6 +11,9 @@ if [ $SYSTEM == "Darwin" ]; then
 elif [ $SYSTEM == "Linux" ]; then
     confDir=sites-enabled
 fi
+
+echo "If this script fails, it may be that ${nginxHome}/${confDir} is not the correct nginx directory on your
+system, in which case you could try running it again with the 'other' (servers/sites-enabled) value for confDir   "
 
 sudo ln -fs $DIR/amiable.conf $nginxHome/$confDir/amiable.conf
 sudo ln -fs $DIR/amiable.crt $nginxHome/amiable.crt
