9a5f54eede03f60a001631d3ec32a4e1f99a014b
Philip McMahon <philip.mcmahon@guardian.co.uk>
2020-12-01 14:36:52 +0000

    Replace hard coded date serialiser with play version.

--- app/models/Serialisation.scala
+++ app/models/Serialisation.scala
@@ -4,6 +4,8 @@ import play.api.libs.json.Json
 
 object Serialisation {
   import utils.DateUtils._
+  import play.api.libs.json.JodaWrites._
+  import play.api.libs.json.JodaReads._
 
   implicit val originFormat = Json.format[Origin]
   implicit val metaFormat = Json.format[Meta]
