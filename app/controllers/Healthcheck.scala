d90865a1c09e35e4de78480dd03220104b042307
Jacob Winch <jacob.winch@guardian.co.uk>
2022-04-26 13:52:07 +0100

    Modify Amiable logging for devx-logs test

--- app/controllers/Healthcheck.scala
+++ app/controllers/Healthcheck.scala
@@ -1,10 +1,12 @@
 package controllers
 
+import play.api.Logging
 import play.api.mvc._
 
-class Healthcheck(val controllerComponents: ControllerComponents) extends BaseController {
+class Healthcheck(val controllerComponents: ControllerComponents) extends BaseController with Logging {
 
   def healthcheck: Action[AnyContent] = Action {
+    logger.info("Healthcheck was successful")
     Ok("ok")
   }
 
