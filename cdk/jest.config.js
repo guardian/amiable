8c7639306fa2f2f80e4ce0a6b79bc13833dbd210
Akash Askoolum <akash1810@gmail.com>
2022-11-08 14:27:09 +0000

    chore: Use Jest v29 snapshot format

--- cdk/jest.config.js
+++ cdk/jest.config.js
@@ -6,12 +6,4 @@ module.exports = {
   testResultsProcessor: "jest-teamcity-reporter",
   transformIgnorePatterns: ["node_modules/(?!@guardian/private-infrastructure-config)"],
   setupFilesAfterEnv: ["./jest.setup.js"],
-
-  // Preserve snapshot format during initial Jest 29 upgrade.
-  // See https://jestjs.io/docs/upgrading-to-jest29.
-  // TODO remove this!
-  snapshotFormat: {
-    escapeString: true,
-    printBasicPrototype: true,
-  },
 };
