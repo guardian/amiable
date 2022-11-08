dd1be4c228729b651f05e99a7003789708b3b048
Akash Askoolum <akash1810@gmail.com>
2022-11-08 14:25:48 +0000

    chore(deps): Update Jest to v29
    Updates:
      - @types/jest from 27.4.1 to 29.2.2
      - jest from 27.3.1 to 29.3.0
      - ts-jest from 28.0.7 to 29.0.3

--- cdk/jest.config.js
+++ cdk/jest.config.js
@@ -6,4 +6,12 @@ module.exports = {
   testResultsProcessor: "jest-teamcity-reporter",
   transformIgnorePatterns: ["node_modules/(?!@guardian/private-infrastructure-config)"],
   setupFilesAfterEnv: ["./jest.setup.js"],
+
+  // Preserve snapshot format during initial Jest 29 upgrade.
+  // See https://jestjs.io/docs/upgrading-to-jest29.
+  // TODO remove this!
+  snapshotFormat: {
+    escapeString: true,
+    printBasicPrototype: true,
+  },
 };
