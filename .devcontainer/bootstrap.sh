1a763e5966eb55b6139577cd532e0176f78a0599
adamnfish-gu <adam.fisher@guardian.co.uk>
2025-08-22 16:11:04 +0100

    Restore some setup steps

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -12,27 +12,27 @@ sudo bash -lc 'apt-get update || (sleep 2 && apt-get update)'
 sudo bash -lc 'apt-get install -y --no-install-recommends ca-certificates curl git ripgrep'
 sudo apt-get clean
 sudo rm -rf /var/lib/apt/lists/*
-#
-## ---- set up mise for this script ----
-#echo -e "\033[1;34m[setup] Setting up mise for this script...\033[0m"
-#eval "$(mise activate bash --shims)"
-#
-## ---- persist mise setup for future bash sessions ----
-#echo 'eval "$(mise activate bash --shims)"' >> ~vscode/.bashrc
-#
-#echo -e "\033[1;34m[setup] mise activated. Current version:\033[0m"
-#mise --version
-#
+
+## ---- set up mise-en-place ----
+echo -e "\033[1;34m[setup] Setting up mise for this script...\033[0m"
+eval "$(mise activate bash --shims)"
+
+echo -e "\033[1;34m[setup] Persisting mise setup for future bash sessions...\033[0m"
+echo 'eval "$(mise activate bash --shims)"' >> ~vscode/.bashrc
+
+echo -e "\033[1;34m[setup] mise activated. Current version:\033[0m"
+mise --version
+
+## ---- make sure mise can use the checked out project's tools definition ----
+mise trust || true
+
 ## Check if mise is activated by inspecting 'mise current' output
 #if mise current | grep -q 'No tools are currently active'; then
 #  echo -e "\033[1;31m[setup] WARNING: mise is on PATH but not activated for this shell\033[0m"
 #else
 #  echo -e "\033[1;32m[setup] mise is activated\033[0m"
 #fi
-#
-#echo -e "\033[1;34m[setup] Persisting mise setup for future bash sessions...\033[0m"
-#mise trust || true
-#
+
 ## installing application toolchain with mise
 #echo -e "\033[1;34m[setup] Installing developer tools...\033[0m"
 #MAX_INSTALL_RETRIES=3
