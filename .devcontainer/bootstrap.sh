10eeb6e55d9598a0ea7df3fbc7af04607c6a5b56
adamnfish-gu <adam.fisher@guardian.co.uk>
2025-09-12 11:56:08 +0100

    Tweak mise setup

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -20,17 +20,16 @@ sudo rm -rf /var/lib/apt/lists/*
 
 ## ---- set up mise-en-place ----
 echo -e "\033[1;34m[setup] Setting up mise for this script...\033[0m"
+# activating with and without shims (not the right approach) for now while we get a prototype working
 eval "$(mise activate bash --shims)"
-eval "$(mise activate bash)"
 
 echo -e "\033[1;34m[setup] Persisting mise setup for future bash sessions...\033[0m"
-echo 'eval "$(mise activate bash --shims)"' >> ~vscode/.bashrc
-echo 'eval "$(mise activate bash)"' >> ~vscode/.bashrc
 
 echo -e "\033[1;34m[setup] mise activated. Current version:\033[0m"
 mise --version
 
 ## ---- make sure mise can use the checked out project's tools definition ----
 mise trust || true
+mise install
 
 echo -e "\033[1;32m========== setup: complete ==========\033[0m"
